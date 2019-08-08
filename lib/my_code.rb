38c699b8322ed4efbb802920c6b3c2745ff3ee79
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 18:27:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,7 @@
 # My Code here....
+def 'my own map-like methods' (values, map_to_negativize)
+  # map_to_negativize returns an array with all values made negative
+
+end
+
+'my own map-like methods'(-900, [1, 2, 3, -9])
